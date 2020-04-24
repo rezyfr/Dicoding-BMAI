1f30b338a9b183a75b40a78c8efb4a8d105c2bae
Fidriyanto Rizkillah <fidriyanto.rizkillah@amartha.com>
2020-04-24 09:30:22 +0700

    Initial Commit

---
+++ Submission/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Submission
+//
+//  Created by Fidriyanto Rizkillah on 24/04/20.
+//  Copyright © 2020 Fidriyanto Rizkillah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
